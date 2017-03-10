f937476b64ec880f4d20a93e869196058e3f67d9
Oleksii Dykan <alick_bass@mail.ru>
2017-03-10 17:46:45 +0100

    Add convenience inits for JSON

--- LucJSON/JSON.swift
+++ LucJSON/JSON.swift
@@ -45,6 +45,36 @@ extension JSON: Equatable {
 }
 
 public extension JSON {
+    public init(_ number: NSNumber) {
+        self = .number(number)
+    }
+    
+    public init(_ int: Int) {
+        self = .number(NSNumber(value: int))
+    }
+    
+    public init(_ double: Double) {
+        self = .number(NSNumber(value: double))
+    }
+    
+    public init(_ bool: Bool) {
+        self = .bool(bool)
+    }
+    
+    public init(_ string: String) {
+        self = .string(string)
+    }
+    
+    public init(_ object: [String: JSON]) {
+        self = .object(object)
+    }
+    
+    public init(_ array: [JSON]) {
+        self = .array(array)
+    }
+}
+
+public extension JSON {
     public var isNull: Bool {
         switch self {
         case .null: return true

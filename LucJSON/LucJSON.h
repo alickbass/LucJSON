1e67677d25d75b69086fb0cc22c584dade0b44f3
Oleksii Dykan <alick_bass@mail.ru>
2017-03-10 11:13:01 +0100

    Initial project setup

---
+++ LucJSON/LucJSON.h
@@ -0,0 +1,19 @@
+//
+//  LucJSON.h
+//  LucJSON
+//
+//  Created by Oleksii on 10/03/2017.
+//  Copyright © 2017 ViolentOctopus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LucJSON.
+FOUNDATION_EXPORT double LucJSONVersionNumber;
+
+//! Project version string for LucJSON.
+FOUNDATION_EXPORT const unsigned char LucJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LucJSON/PublicHeader.h>
+
+

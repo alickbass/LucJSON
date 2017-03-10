690b920462beef5e74410ea4e22aece5077c0ece
Oleksii Dykan <alick_bass@mail.ru>
2017-03-10 17:47:01 +0100

    Cover inits with tests

--- LucJSONTests/JSONTests.swift
+++ LucJSONTests/JSONTests.swift
@@ -12,15 +12,17 @@ import LucJSON
 class JSONTests: XCTestCase {
     
     func testJSONEquatable() {
-        XCTAssertEqual(JSON.bool(true), .bool(true))
-        XCTAssertNotEqual(JSON.bool(true), .bool(false))
-        XCTAssertEqual(JSON.object(["firstName": .null]), .object(["firstName": .null]))
-        XCTAssertNotEqual(JSON.object(["lastName": .null]), .object(["firstName": .null]))
-        XCTAssertEqual(JSON.string("lastName"), .string("lastName"))
-        XCTAssertEqual(JSON.number(2), .number(2.0))
-        XCTAssertNotEqual(JSON.number(2), .number(2.2))
-        XCTAssertNotEqual(JSON.array([.null, .number(2)]), .array([.null, .null]))
-        XCTAssertNotEqual(JSON.number(2), .string("lastName"))
+        XCTAssertEqual(JSON(true), .bool(true))
+        XCTAssertNotEqual(JSON(true), .bool(false))
+        XCTAssertEqual(JSON(["firstName": .null]), .object(["firstName": .null]))
+        XCTAssertNotEqual(JSON(["lastName": .null]), .object(["firstName": .null]))
+        XCTAssertEqual(JSON("lastName"), .string("lastName"))
+        XCTAssertEqual(JSON(2), .number(2.0))
+        XCTAssertEqual(JSON(NSNumber(value: 2)), .number(2.0))
+        XCTAssertEqual(JSON(2.0), .number(2.0))
+        XCTAssertNotEqual(JSON(2), .number(2.2))
+        XCTAssertNotEqual(JSON([.null, .number(2)]), .array([.null, .null]))
+        XCTAssertNotEqual(JSON(2), .string("lastName"))
     }
     
     func testOptionalValues() {
@@ -38,5 +40,4 @@ class JSONTests: XCTestCase {
         XCTAssertNil(JSON.number(4).object)
         XCTAssertNil(JSON.number(false).array)
     }
-    
 }
